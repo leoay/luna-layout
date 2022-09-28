0cae728a043c85bb9901e6ec2999ef422360fcba
yaoshuai <yaoshuai@iplusbot.com>
2022-09-28 11:39:31 +0800

    update

--- internal/app/schema/greet.go
+++ internal/app/schema/greet.go
@@ -1,21 +1,9 @@
 package schema
 
-import (
-	"fmt"
-	"time"
-)
-
 // Greet 角色对象
 type Greet struct {
-	ID         uint64     `json:"id,string"`                             // 唯一标识
-	Name       string     `json:"name" binding:"required"`               // 角色名称
-	Sequence   int        `json:"sequence"`                              // 排序值
-	Memo       string     `json:"memo"`                                  // 备注
-	Status     int        `json:"status" binding:"required,max=2,min=1"` // 状态(1:启用 2:禁用)
-	Creator    uint64     `json:"creator"`                               // 创建者
-	CreatedAt  time.Time  `json:"created_at"`                            // 创建时间
-	UpdatedAt  time.Time  `json:"updated_at"`                            // 更新时间
-	GreetMenus GreetMenus `json:"Greet_menus" binding:"required,gt=0"`   // 角色菜单列表
+	ID   uint64 `json:"id,string"`               // 唯一标识
+	Name string `json:"name" binding:"required"` // 角色名称
 }
 
 // GreetQueryParam 查询条件
@@ -24,7 +12,6 @@ type GreetQueryParam struct {
 	IDs        []uint64 `form:"-"`          // 唯一标识列表
 	Name       string   `form:"-"`          // 角色名称
 	QueryValue string   `form:"queryValue"` // 模糊查询
-	Status     int      `form:"status"`     // 状态(1:启用 2:禁用)
 }
 
 // GreetQueryOptions 查询可选参数项
@@ -58,84 +45,4 @@ func (a Greets) ToMap() map[uint64]*Greet {
 		m[item.ID] = item
 	}
 	return m
-}
-
-// ----------------------------------------GreetMenu--------------------------------------
-
-// GreetMenu 角色菜单对象
-type GreetMenu struct {
-	ID       uint64 `json:"id,string"`                           // 唯一标识
-	GreetID  uint64 `json:"Greet_id,string" binding:"required"`  // 角色ID
-	MenuID   uint64 `json:"menu_id,string" binding:"required"`   // 菜单ID
-	ActionID uint64 `json:"action_id,string" binding:"required"` // 动作ID
-}
-
-// GreetMenuQueryParam 查询条件
-type GreetMenuQueryParam struct {
-	PaginationParam
-	GreetID  uint64   // 角色ID
-	GreetIDs []uint64 // 角色ID列表
-}
-
-// GreetMenuQueryOptions 查询可选参数项
-type GreetMenuQueryOptions struct {
-	OrderFields  []*OrderField
-	SelectFields []string
-}
-
-// GreetMenuQueryResult 查询结果
-type GreetMenuQueryResult struct {
-	Data       GreetMenus
-	PageResult *PaginationResult
-}
-
-// GreetMenus 角色菜单列表
-type GreetMenus []*GreetMenu
-
-// ToMap 转换为map
-func (a GreetMenus) ToMap() map[string]*GreetMenu {
-	m := make(map[string]*GreetMenu)
-	for _, item := range a {
-		m[fmt.Sprintf("%d-%d", item.MenuID, item.ActionID)] = item
-	}
-	return m
-}
-
-// ToGreetIDMap 转换为角色ID映射
-func (a GreetMenus) ToGreetIDMap() map[uint64]GreetMenus {
-	m := make(map[uint64]GreetMenus)
-	for _, item := range a {
-		m[item.GreetID] = append(m[item.GreetID], item)
-	}
-	return m
-}
-
-// ToMenuIDs 转换为菜单ID列表
-func (a GreetMenus) ToMenuIDs() []uint64 {
-	var idList []uint64
-	m := make(map[uint64]struct{})
-
-	for _, item := range a {
-		if _, ok := m[item.MenuID]; ok {
-			continue
-		}
-		idList = append(idList, item.MenuID)
-		m[item.MenuID] = struct{}{}
-	}
-
-	return idList
-}
-
-// ToActionIDs 转换为动作ID列表
-func (a GreetMenus) ToActionIDs() []uint64 {
-	idList := make([]uint64, len(a))
-	m := make(map[uint64]struct{})
-	for i, item := range a {
-		if _, ok := m[item.ActionID]; ok {
-			continue
-		}
-		idList[i] = item.ActionID
-		m[item.ActionID] = struct{}{}
-	}
-	return idList
 }
--- internal/app/service/greet.srv.go
+++ internal/app/service/greet.srv.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"github.com/google/wire"
 
-	"github.com/leoay/luna/pkg/errors"
-	"github.com/leoay/luna/pkg/util/snowflake"
 	"luna-layout/internal/app/dao"
 	"luna-layout/internal/app/schema"
 )
@@ -22,91 +20,17 @@ func (a *GreetSrv) Query(ctx context.Context, params schema.GreetQueryParam, opt
 }
 
 func (a *GreetSrv) Get(ctx context.Context, id uint64, opts ...schema.GreetQueryOptions) (*schema.Greet, error) {
-	item, err := a.GreetRepo.Get(ctx, id, opts...)
-	if err != nil {
-		return nil, err
-	} else if item == nil {
-		return nil, errors.ErrNotFound
-	}
-	return item, nil
+	return nil, nil
 }
 
 func (a *GreetSrv) Create(ctx context.Context, item schema.Greet) (*schema.IDResult, error) {
-	item.ID = snowflake.MustID()
-	err = a.TransRepo.Exec(ctx, func(ctx context.Context) error {
-		for _, rmItem := range item.GreetMenus {
-			rmItem.ID = snowflake.MustID()
-			rmItem.GreetID = item.ID
-			err := a.GreetMenuRepo.Create(ctx, *rmItem)
-			if err != nil {
-				return err
-			}
-		}
-		return a.GreetRepo.Create(ctx, item)
-	})
-	if err != nil {
-		return nil, err
-	}
-	return schema.NewIDResult(item.ID), nil
+	return nil, nil
 }
 
 func (a *GreetSrv) Update(ctx context.Context, id uint64, item schema.Greet) error {
-	oldItem, err := a.Get(ctx, id)
-	if err != nil {
-		return err
-	} else if oldItem == nil {
-		return errors.ErrNotFound
-	} else if oldItem.Name != item.Name {
-		err := a.checkName(ctx, item)
-		if err != nil {
-			return err
-		}
-	}
-
-	item.ID = oldItem.ID
-	item.Creator = oldItem.Creator
-	item.CreatedAt = oldItem.CreatedAt
-	err = a.TransRepo.Exec(ctx, func(ctx context.Context) error {
-		addGreetMenus, delGreetMenus := a.compareGreetMenus(ctx, oldItem.GreetMenus, item.GreetMenus)
-		for _, rmitem := range addGreetMenus {
-			rmitem.ID = snowflake.MustID()
-			rmitem.GreetID = id
-			err := a.GreetMenuRepo.Create(ctx, *rmitem)
-			if err != nil {
-				return err
-			}
-		}
-
-		for _, rmitem := range delGreetMenus {
-			err := a.GreetMenuRepo.Delete(ctx, rmitem.ID)
-			if err != nil {
-				return err
-			}
-		}
-
-		return a.GreetRepo.Update(ctx, id, item)
-	})
-	if err != nil {
-		return err
-	}
 	return nil
 }
 
 func (a *GreetSrv) Delete(ctx context.Context, id uint64) error {
-	oldItem, err := a.GreetRepo.Get(ctx, id)
-	if err != nil {
-		return err
-	} else if oldItem == nil {
-		return errors.ErrNotFound
-	}
-	GreetResult, err := a.GreetRepo.Query(ctx, schema.GreetQueryParam{
-		PaginationParam: schema.PaginationParam{OnlyCount: true},
-		GreetIDs:        []uint64{id},
-	})
-	if err != nil {
-		return err
-	} else if GreetResult.PageResult.Total > 0 {
-		return errors.New400Response("不允许删除已经存在用户的角色")
-	}
 	return nil
 }
