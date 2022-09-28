2096836dc0d8a12064f05d1420ebd9a462d3ba94
yaoshuai <yaoshuai@iplusbot.com>
2022-09-28 10:31:34 +0800

    update

--- internal/app/dao/dao.go
+++ internal/app/dao/dao.go
@@ -23,7 +23,7 @@ type (
 	GreetRepo = Greet.GreetRepo
 ) // end
 
-// Auto migration for given models
+// AutoMigrate Auto migration for given models
 func AutoMigrate(db *gorm.DB) error {
 	if dbType := config.C.Gorm.DBType; strings.ToLower(dbType) == "mysql" {
 		db = db.Set("gorm:table_options", "ENGINE=InnoDB")
